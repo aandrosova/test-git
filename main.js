6f1e553b0c5871814bbe138dd9ecfc1ab2cf33d3
aandrosova <aandrosova@tradingview.com>
2022-10-15 22:24:28 +0300

    my first calc

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-let answer = prompt('Кто там?', '');
-if(answer == 'Админ') {
-    let check = prompt('Пароль?', '');
-    if(check == 'Отмена') {
-        alert('ОТМЕНЕНО');
-    } else if(check == 'Я главный') {
-        alert('Здравствуйте!');
+function calc(action, a, b) {
+    action = prompt('Введите необходимое действие', '');
+    a = +prompt('Введите первое число', '');
+    b = +prompt('Введите второе число', '');
+    if (action === "add") {
+        return alert(a + b);
+    } else if (action === "multi") {
+        return alert(a * b);
+    } else if (action === "subtract") {
+        return alert(a - b);
     } else {
-        alert('Неверный пароль!');
+        return alert('Вы ввели неверные значения');
     }
-} else if(answer == 'Отмена') {
-    alert('ОТМЕНЕНООООО!');
-} else {
-    alert('Не путю!');
-   
 }
 
+calc();
